1309815ffd1ab68e0f9c324297b0ca66351307de
KaiLiu12 <74182067+onetrickollie@users.noreply.github.com>
2026-03-12 16:12:42 -0700

    skeleton code for DCT

---
+++ ImageDisplay_C++_cross_platform/src/DCT.cpp
@@ -0,0 +1,13 @@
+#include "DCT.h"
+
+// TODO Phase 2: Implement DCT and IDCT
+
+void computeDCT(std::vector<std::vector<double>>& block, int N)
+{
+    // Implementation coming in Phase 2
+}
+
+void computeIDCT(std::vector<std::vector<double>>& block, int N)
+{
+    // Implementation coming in Phase 2
+}
